7fba1b69c75a6f578824b257c618ecd22a9f37d1
haheha24 <haheha24@users.noreply.github.com>
2021-05-21 00:15:39 +1000

    added body-parser. attempted to use app.get to connect to lookupdb.js but I am clearly not understanding what is happening at this stage. Refactored Form.js slightly

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 console.log('this is my server')
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
 app.listen(3000, () => {
     console.log('Listening on 3000')
+})
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/lookupdb.js')
+    console.log(req.body + "lookup")
+    console.log(__dirname)
+})
+
+app.post('/names', (req, res) => {
+    console.log(req.body)
 })
