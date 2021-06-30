f85c9888cbb88f563b06ab1f703c939bf3a76f56
haheha24 <haheha24@users.noreply.github.com>
2021-06-30 22:00:09 +1000

    created a post request to users.js route that imports the users-model schema and saves it to the lookup user collection

--- model/users-model.js
+++ model/users-model.js
@@ -1,7 +1,35 @@
 const mongoose = require("mongoose");
 
+const schema = new mongoose.Schema(
+  {
+    firstName: String,
+    lastName: String,
+    email: String,
+  },
+  { collection: "user" }
+);
+const User = mongoose.model("user", schema);
+
+module.exports = User;
+
+/* module.exports = (first, last, address) => {
+  User.insertMany(
+    [
+      {
+        firstName: first,
+        lastName: last,
+        email: address,
+      },
+    ],
+    (err, r) => {
+      test.equal(null, err);
+      test.equal(2, r.insertedCount);
+    }
+  );
+}; */
+
 // Open the connection once by creating a schema and log the user collection
-module.exports = mongoose.connection.once("open", function () {
+/* module.exports = mongoose.connection.once("open", function () {
   console.log(`Connected to the database`);
 
   const userSchema = new mongoose.Schema(
@@ -10,4 +38,4 @@ module.exports = mongoose.connection.once("open", function () {
   );
 
   const User = mongoose.model("user", userSchema);
-});
+}); */
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
-const userModel = require('../model/users-model.js')
-/* router.use('/', userModel) */
+router.use(express.json()); //For JSON requests
+router.use(express.urlencoded({ extended: true }));
+const userModel = require("../model/users-model.js");
 
-router.post('/', (req, res) => {
-    res.send('hello')
-})
+router.post("/add", (req, res, next) => {
+  console.log(req.body);
+  let newUser = new userModel({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+  });
+  newUser.save((err, newUser) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send({
+        status: 200,
+        message: "Successfully added document",
+        userObj: newUser,
+      });
+    }
+  });
+});
 
-module.exports = router
+module.exports = router;
