15d2fa08752cf794b009e4f884c96d425012abd8
haheha24 <haheha24@users.noreply.github.com>
2021-06-30 22:29:49 +1000

    removed comments

--- src/components/Display.js
+++ src/components/Display.js
@@ -34,44 +34,6 @@ const Display = ({ stateData, deleteData }) => {
   } else {
     return <li className="dataList">There is no Data :(</li>;
   }
-
-  /* return (
-    <div className="display-container">
-      <ul className="dataUlist">
-        {stateData.length > 1 ? (
-          stateData.map((storedData, index) => {
-            return (
-              <li key={storedData.id} className="dataList">
-                {storedData.firstName} {storedData.lastName}{" "}
-                <FaTimes
-                  style={{
-                    color: "red",
-                    cursor: "pointer",
-                    verticalAlign: "middle",
-                  }}
-                  onClick={() => deleteData(stateData[index].id)}
-                />
-              </li>
-            );
-          })
-        ) : stateData.length === 1 ? (
-          <li className="dataList">
-            {stateData[0].firstName} {stateData[0].lastName}{" "}
-            <FaTimes
-              style={{
-                color: "red",
-                cursor: "pointer",
-                verticalAlign: "middle",
-              }}
-              onClick={() => deleteData(stateData[0].id)}
-            />
-          </li>
-        ) : stateData.length === 0 ? (
-          <li className="dataList">There is no Data :(</li>
-        ) : null}
-      </ul>
-    </div>
-  ); */
 };
 
 export default Display;
