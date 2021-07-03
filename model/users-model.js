4d239a02fcce1afd6d9b3f62e1dfcad944af916a
haheha24 <haheha24@users.noreply.github.com>
2021-07-03 14:59:15 +1000

    removed comments

--- model/users-model.js
+++ model/users-model.js
@@ -11,31 +11,3 @@ const schema = new mongoose.Schema(
 const User = mongoose.model("user", schema);
 
 module.exports = User;
-
-/* module.exports = (first, last, address) => {
-  User.insertMany(
-    [
-      {
-        firstName: first,
-        lastName: last,
-        email: address,
-      },
-    ],
-    (err, r) => {
-      test.equal(null, err);
-      test.equal(2, r.insertedCount);
-    }
-  );
-}; */
-
-// Open the connection once by creating a schema and log the user collection
-/* module.exports = mongoose.connection.once("open", function () {
-  console.log(`Connected to the database`);
-
-  const userSchema = new mongoose.Schema(
-    { firstName: String, secondName: String, email: String },
-    { collection: "user" }
-  );
-
-  const User = mongoose.model("user", userSchema);
-}); */
